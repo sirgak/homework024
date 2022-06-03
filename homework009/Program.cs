d4944e904be4185c1c6edda5e1f568e7081fadeb
sirgak <life_4_ever@mail.ru>
2022-06-03 19:37:03 +0300

    test
    test

---
+++ homework009/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homework009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "ключ";
+            int tryCount = 3;
+
+            for (int i=0; i < tryCount; i++)
+            {
+                Console.Write("пароль: ");
+                string userInput=Console.ReadLine();
+
+                if (password == userInput )
+                {
+                    Console.WriteLine("Секрет ");
+                }
+                else
+                {
+                    Console.WriteLine("Попыток осталось " + (tryCount - i - 1));
+                }
+            }
+        }
+    }
+}
